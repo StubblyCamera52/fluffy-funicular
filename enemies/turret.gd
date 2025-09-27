7f68a347b8fc7fc42b21cc46eeff79b076e0e5e8
Gavan <gavanbess@gmail.com>
2025-09-27 15:32:07 -0700

    turret

--- enemies/turret.gd
+++ enemies/turret.gd
@@ -1,5 +1,9 @@
 extends Enemy
 
+var target_player = null
+var target_rotation = 0
+@export_range(0,5) var ROTATE_SPEED := 5
+
 func take_damage(dmg_amount: int) -> void:
 	health -= dmg_amount
 	if health <= 0:
@@ -21,7 +25,23 @@ func set_movement_target(movement_target: Vector3):
 func _physics_process(delta: float) -> void:
 	velocity = Vector3.ZERO
 	
+	if target_player != null:
+		var player_pos: Vector3 = target_player.global_position
+		var direction: float = PI/2-Vector2(global_position.direction_to(player_pos).x,global_position.direction_to(player_pos).z).angle()
+		rotation.y = lerp_angle(rotation.y, direction, ROTATE_SPEED*delta)
+		
+	
 	if !is_on_floor():
 		velocity = Vector3(0, -1, 0)
 	
 	move_and_slide()
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body.name == "Player":
+		target_player = body
+
+
+func _on_area_3d_body_exited(body: Node3D) -> void:
+	if body == target_player:
+		target_player = null
