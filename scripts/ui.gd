a736dd34324e0d3a1bd9622c259c2d5d72cb29de
Gavan <gavanbess@gmail.com>
2025-09-28 10:46:09 -0700

    ability unlock ui

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -4,12 +4,38 @@ func _init() -> void:
 	PlayerGlobalManager.took_damage.connect(update_player_health_ui)
 	PlayerGlobalManager.xp_changed.connect(update_player_level_ui)
 	PlayerGlobalManager.xp_changed.connect(update_xp_next_level_ui)
+	PlayerGlobalManager.xp_changed.connect(update_ability_ui)
 
 func update_player_health_ui() -> void:
-	$VBoxContainer/health.text = str("Health: "+str(PlayerGlobalManager.player_health))
+	$MarginContainer/VBoxContainer/health.text = str("Health: "+str(PlayerGlobalManager.player_health))
 
 func update_player_level_ui() -> void:
-	$VBoxContainer/level.text = str("Level: "+str(PlayerGlobalManager.player_level))
+	$MarginContainer/VBoxContainer/level.text = str("Level: "+str(PlayerGlobalManager.player_level))
 
 func update_xp_next_level_ui() -> void:
-	$VBoxContainer/xpToNextLevel.text = str("XP to next level: "+str(5-(PlayerGlobalManager.player_xp-((PlayerGlobalManager.player_level*5)-5))))
+	$MarginContainer/VBoxContainer/xpToNextLevel.text = str("XP to next level: "+str(5-(PlayerGlobalManager.player_xp-((PlayerGlobalManager.player_level*5)-5))))
+
+var previous_level := 1
+
+func update_ability_ui() -> void:
+	if PlayerGlobalManager.player_level > previous_level:
+		match PlayerGlobalManager.player_level:
+			2:
+				$"Ability Unlock".text = '''[shake rate=20.0 level=5 connected=1][b]New Ability Unlocked:[/b][/shake]\n[color=green]Double Jump[/color]\nJump in the air to activate'''
+				$"Ability Unlock/DisplayTimer".start()
+			3:
+				$"Ability Unlock".text = '''[shake rate=20.0 level=5 connected=1][b]New Ability Unlocked:[/b][/shake]\n[color=green]Wall Jump[/color]\nJump off a wall to activate'''
+				$"Ability Unlock/DisplayTimer".start()
+			4:
+				$"Ability Unlock".text = '''[shake rate=20.0 level=5 connected=1][b]New Ability Unlocked:[/b][/shake]\n[color=green]Triple Jump[/color]\nJump in the air to activate'''
+				$"Ability Unlock/DisplayTimer".start()
+			5:
+				$"Ability Unlock".text = '''[shake rate=20.0 level=5 connected=1][b]New Ability Unlocked:[/b][/shake]\n[color=green]Nothing[/color]\n In progess'''
+				$"Ability Unlock/DisplayTimer".start()
+			_:
+				pass
+	previous_level = PlayerGlobalManager.player_level
+
+
+func _on_display_timer_timeout() -> void:
+	$"Ability Unlock".text = ""
