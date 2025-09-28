45bf085f0713de063f9c9445ae9f2d95e0a780ad
TaelsDaFoox <bazmulligan@gmail.com>
2025-09-28 11:40:43 -0700

    improved player animations

--- assets/objects/sacrifice_door_model.gd
+++ assets/objects/sacrifice_door_model.gd
@@ -5,8 +5,8 @@ var opened = false
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if not opened and PlayerGlobalManager.player_level>=2:
-		PlayerGlobalManager.player_level-=1
 		PlayerGlobalManager.player_xp=PlayerGlobalManager.player_level*5-5
+		PlayerGlobalManager.give_player_xp(0)
 		PlayerGlobalManager.sacrifice_animation(global_position)
 		opened=true
 		await get_tree().create_timer(4.5).timeout
